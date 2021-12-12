3ebc9efcafd9161b11ef1142c3607dc4f17d8de5
alexandra <6198010@gmail.com>
2021-12-12 12:04:37 +0200

    fixed main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-if (window.location.pathname === '/foreTeller'){
+  window.homepagecheck = function() {
+   var check = false;
+   if(document.location.pathname === "/"){
+     check=true;
+     }
+   return check;
+ }
+if (window.homepagecheck){
     window.setTimeout(function(){
        window.location.href = "/foreTeller/step1.html";
 
@@ -12,3 +19,10 @@ if (window.location.pathname === '/foreTeller/graphic.html'){
   }, 5000);
 }
 
+
+let list = document.getElementsByClassName('rating-area');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'label') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
